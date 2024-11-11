1b69406e958e522829fecd90b82ba3a74906de4b
debiprmstyle <debipramesty@gmail.com>
2024-11-11 23:17:41 +0800

    progress 5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Pastikan untuk mengimpor ini jika menggunakan npm
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Ensure this is imported if using npm
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
     return (
         <section className="hero bg-cover bg-center h-screen text-white flex flex-col items-start md:mx-32 mx-0 rounded-xl px-4 relative" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1482029255085-35a4a48b7084?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDcxfHx8ZW58MHx8fHx8")' }}>
-            <div className="w-full md:max-w-lg text-left mb-8 mt-24 md:mt-48 md:ml-6">
+            {/* Black overlay */}
+            <div className="absolute inset-0 bg-black opacity-50 rounded-xl"></div>
+
+            <div className="w-full md:max-w-lg text-left mb-8 mt-24 md:mt-48 md:ml-6 z-10">
                 <h1 className="font-bold mb-4"><span className='md:text-6xl text-5xl'>We always aim for the stars.</span></h1>
                 <p className="text-xl mb-8">Join us and discover the journey.</p>
                 {/* <button className="bg-teal-800 hover:bg-teal-900 px-6 py-3 rounded-full text-lg">Get Started</button> */}
